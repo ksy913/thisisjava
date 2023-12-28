ea4dbd89bb9e397be46adf8a715da65ffb7986f3
ksy <ksy960913@gmail.com>
2023-12-28 16:52:01 +0900

    1

---
+++ src/ch03/ex3_3.java
@@ -0,0 +1,24 @@
+package ch03;
+
+public class ex3_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte var1 =125;
+		for(int i=0; i<5; i++) {	// {}를 5번 반복 실행
+			var1++;					// ++ 연산은 var1의 값을 1 증가 시킨다.
+			
+			System.out.println("var1: " + var1);
+		}
+		
+		System.out.println("------------------------");
+		
+		byte var2 = -125;
+		for(int i=0; i<5; i++) {	// {}를 5번 반복 실행
+			var2--;					// -- 연산은 var2의 값을 1 증가 시킨다.
+			System.out.println("var2: " + var2);
+		}
+	}
+
+}
